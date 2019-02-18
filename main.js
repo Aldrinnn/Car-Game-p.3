aa8dc2dce4a333de8e7690304c86a975ea9881e1
Aldrin Portillo <Aldrin@Aldrins-MacBook-Air.local>
2019-02-18 18:21:24 -0500

    Fixed Js

--- main.js
+++ main.js
@@ -1,36 +1,72 @@
-//Variables
+// VARIABLES
+const car = {
+    make: 'Hot Wheels',
+    model: 'Skull',
+    color: 'Orange',
+    mileage: 0,
+    isWorking: true,
 
-let car ={
-	make: 'hotwheels',
-	model:'Skull',
-	color:'orange',
-	mileage:0,
-	isWoring: true,
+    driveToWork(){
+        alert(`Old Mileage: ${this.mileage}`);
+        this.mileage += 10;
+        alert(`New Mileage: ${this.mileage}`);
+    },
 
-	driveTowork(){
-		alert('Old mileage: $(this.mileage)');
-		this.mileage = this.mileage + 8;
-		alert('New mileage: $(this.mileage)');
-	},
+    driveAroundUSA(){
+        alert(`Old Mileage: ${this.mileage}`);
+        this.mileage += 7000;
+        alert(`New Mileage: ${this.mileage}`);
+        alert('Car needs a tuneup!');
+        this.isWorking = false;
+    },
+
+    getTuneUp(){
+        alert('Car is fixed and ready to go!');
+        this.isWorking = true;
+    },
+
+    honk(){
+        alert("Honk! Honk!");
+    }
+};
+
+// FUNCTIONS
+// Logs all of our car's current stats to the console
+reWriteStats=()=>{
+    console.log(`Make: ${car.make}`);
+    console.log(`Model: ${car.model}`);
+    console.log(`Color: ${car.color}`);
+    console.log(`Mileage: ${car.mileage}`);
+    console.log(`IsWorking: ${car.isWorking}`);
+    console.log('-----------------------------');
+
 }
 
 
-//Functions
-	Const rewritestats(){
-
-	}
-
-
-
-/*Captures keyboard input. Depending on the letter pressed it
-will "call" (execute) different functions.*/
+// MAIN PROCESS
+// Captures keyboard input. Depending on the letter pressed it will "call" (execute) different functions.
 document.onkeyup = (event) => {
+    // Captures the key press, converts it to lowercase, and saves it to a variable
+    let letter = String.fromCharCode(event.keyCode).toLowerCase();
 
-	//Captures the key press, converts it to a lowercase, and saves it to a variable.
-	let letter = string.fromCharcode(event.Keycode).tolowercase();
+    if (letter === "d") {
+        car.driveToWork();
+        reWriteStats();
+    }
 
-	if(letter === 'd'){
-		car.driveTowork();
-		rewritestats();
-	}
+    if (letter === "r") {
+        car.driveAroundUSA();
+        reWriteStats();
+    }
+
+    if (letter === "t") {
+        car.getTuneUp();
+        reWriteStats();
+    }
+
+    if (letter === "h") {
+        car.honk();
+        reWriteStats();
+    }
+
 }
